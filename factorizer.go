7e94074af9f58d50a666585d288a4f1249406ade
Tobias Martine <tobias.martine@stud.tu-darmstadt.de>
2021-05-01 14:42:31 +0200

    initial guess implemented

--- factorizer.go
+++ factorizer.go
@@ -2,8 +2,32 @@ package main
 
 import(
 	"fmt"
+	"math"
 )
 
+/**
+	We have 4 important variables:
+		1. N: The number we want to factorize
+		2. p: The first factor of N
+		3. q: The second factor of N
+		4. step_width: How many steps we make from the current position
+		5. (a,b): Current position
+**/
+
+var N int
+var p int
+var q int
+
 func main()  {
-	fmt.Println("Hello World!")
+	p := 31
+	q := 7
+	N := p * q
+
+	a, b := initial_guess(N)
+	fmt.Println("Hello World!", a, b)
+}
+
+func initial_guess(N int) (a_init int, b_init int) {
+	guess := int( math.Round( math.Sqrt( float64(N) ) ) )
+	return guess, guess
 }
