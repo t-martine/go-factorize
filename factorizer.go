96c52abba894751a87ec6f8b00937e2047b5b10d
Tobias Martine <tobias.martine@stud.tu-darmstadt.de>
2021-06-06 13:26:55 +0200

    fix: factorizer now handles big.Int :)

--- factorizer.go
+++ factorizer.go
@@ -2,14 +2,13 @@ package main
 
 import(
 	"fmt"
-	"math"
+	"math/big"
 	"os"
-	"strconv"
 )
 
 type point struct {
-	a int
-	b int	
+	a big.Int
+	b big.Int	
 }
 /**
 	We have 4 important variables:
@@ -21,7 +20,6 @@ type point struct {
 **/
 
 var steps int
-
 func main()  {
 
 	steps = 0;
@@ -29,23 +27,23 @@ func main()  {
 		fmt.Println("You need to proide a number N to factorize as a command line argument!")
 		return
 	}
-	arg := os.Args[1]
+	string_arg := os.Args[1]
 
-	num, _ := strconv.Atoi(arg)
-	
+	num := big.NewInt(0)
+	num.SetString( string_arg , 10)
 	fmt.Println("Finding prime Factors for", num)
-	find_factors(num)
+	find_factors(*num)
 
 	fmt.Println("Took", steps, "steps")
 }
 
-func find_factors(N int) (int){
-	done := make(chan int)
+func find_factors(N big.Int) (int){
+	done := make(chan big.Int)
 	
-	upper_bound := 1
-	primes := sieveOfEratosthenes(upper_bound)
-	primes = append(primes, 1)
-	//primes =  int[]{1}
+	var upper_bound int64 = 1
+	//primes := sieveOfEratosthenes(upper_bound)
+	//primes = append(primes, 1)
+	primes := []big.Int{ *big.NewInt(upper_bound) }
 
 	for _,s:= range primes {
 		go execute_algorithm(s, done, N)
@@ -53,71 +51,77 @@ func find_factors(N int) (int){
 	fmt.Println("Started", len(primes), "Workers")
 	
 	lucky_worker := <- done
-	fmt.Println("Worker with step size:", lucky_worker, "found it")
+	fmt.Println("Worker with step size:", lucky_worker.String(), "found it")
 
 	return 0;
 }
 
-func execute_algorithm(step_len int, done chan int, N int) {
+func execute_algorithm(step_len big.Int, done chan big.Int, N big.Int) {
 
 	guess_init := initial_guess(N)
 	current_point := guess_init
 	last_point := guess_init
 	next_point := guess_init
-
-	for ; calculate_distance(current_point, N) != 0;  {
+	dist := big.NewInt(1)
+	for ; dist.Cmp( big.NewInt(0)) != 0;  {
 		
 		next_point = make_step(current_point, last_point, step_len, N)
 		last_point = current_point
 		current_point = next_point
 		steps = steps + 1
-
+		res := calculate_distance(current_point, N)
+		dist = &res
 	}
 	
-	fmt.Println("Found the factors!", current_point.a, current_point.b)
+	fmt.Println("Found the factors!", current_point.a.String(), current_point.b.String())
 	done <- step_len
 }
 
-func initial_guess(N int) (point) {
-	coord := int( math.Round( math.Sqrt( float64(N) ) ) )
-	return point{ a:coord, b:coord}
+func initial_guess(N big.Int) (point) {
+	coord := big.NewInt(0)
+	coord = coord.Sqrt( &N )  
+	return point{ a: *coord, b: *coord}
 }
 
-func make_step(current_position point, last_position point, step_len int, N int) (next_position point)  {
+func make_step(current_position point, last_position point, step_len big.Int, N big.Int) (next_position point)  {
 	
 	// Imagine standing in a 2D-plane where (a,b) is a point and abs( N - a*b ) is its value
 	// The plane goes from top left to bottom right (like in a excel sheet) where (p, ) are the columns and (, q) the rows
 
+	b_up, _   := big.NewInt(0).SetString(current_position.b.String(), 10)
+	b_up 	   = b_up.Add(b_up, &step_len)
+	a_down, _ := big.NewInt(0).SetString(current_position.a.String(), 10)
+	a_down     = a_down.Sub(a_down, &step_len)
 	// 1. Step to the bottom -> (a, b+1)
-	step_bottom := point{ a: current_position.a, b: current_position.b + step_len}
+	step_bottom := point{ a: current_position.a, b: *b_up}
 
 	// 2. Step to the left -> (a-1, b)
-	step_left := point{ a: current_position.a - step_len, b: current_position.b}
+	step_left := point{ a: *a_down, b: current_position.b}
 
-
+	
 	dist_bottom := calculate_distance(step_bottom, N)
 	dist_left := calculate_distance(step_left, N)
 
-	distances := []int{dist_bottom, dist_left}
+	distances := []big.Int{dist_bottom, dist_left}
 	
 	min_dist := Min(distances)
 
-
-	if dist_bottom == min_dist {
+	if dist_bottom.Cmp(&min_dist) == 0 {
 		return step_bottom
 	} else  {
 		return step_left
 	}
 }
 
-func calculate_distance(point point, N int) (int){
-	return int(math.Abs(float64(N - point.a * point.b )))
+func calculate_distance(point point, N big.Int) (big.Int){
+	res := big.NewInt(0).Abs( big.NewInt(0).Sub( &N, big.NewInt(0).Mul( &point.a, &point.b )))
+	return *res
 }
 
-func Min(array []int) (int) {
-    var min int = array[0]
+func Min(array []big.Int) (big.Int) {
+    var min big.Int = array[0]
     for _, value := range array {
-        if min > value {
+        if min.Cmp(&value) > 0 {
             min = value
         }
     }
